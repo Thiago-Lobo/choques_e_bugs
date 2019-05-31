8ca2dee32968752131dfdf19abc7067eb8c38bcc
Thiago Costa Lobo <thiagolobo@poli.ufrj.br>
2019-05-31 08:40:46 -0300

    Adiciona codigo do video sobre Comunicacao Serial

---
+++ cb_2_comunicacao_serial/hello_world/hello_world.ino
@@ -0,0 +1,14 @@
+// Preparar...
+void setup() {
+  Serial.begin(115200); 
+}
+
+// Executar!
+void loop() {
+//  Serial.print("Choques ");
+//  Serial.print("e ");
+//  Serial.println("Bugs\n");
+
+  Serial.print("1\n2\n3\n");
+  delay(1000);
+}
---
+++ cb_2_comunicacao_serial/print_vs_println/print_vs_println.ino
@@ -0,0 +1,14 @@
+// Preparar...
+void setup() {
+  Serial.begin(115200);
+}
+
+// Executar!
+void loop() {
+//  Serial.println("Choques e bugs!");
+  
+  Serial.print("Choques");
+  Serial.print(" e ");
+  Serial.println("Bugs!");
+  delay(1000);
+}
